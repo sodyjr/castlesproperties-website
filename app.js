cc1a8da3f2ea85ae64d4737b5dda93e0f1e08d2f
sodyjr <124619061+sodyjr@users.noreply.github.com>
2023-02-08 01:03:05 -0800

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var menu = document.querySelector('#mobile-menu')
-var menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
